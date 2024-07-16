a90bf05cfc19bb894e08371a510534db2595d80c
halibobo1205 <halibobo1205@gmail.com>
2024-07-16 21:07:50 +0800

    feat(arch): support arm on jdk17

---
+++ platform/src/main/java/x86/org/tron/common/math/MathWrapper.java
@@ -0,0 +1,15 @@
+package org.tron.common.math;
+
+/**
+ * This class is deprecated and should not be used in new code,
+ * for cross-platform consistency, please use {@link StrictMathWrapper} instead,
+ * especially for floating-point calculations.
+ */
+@Deprecated
+public class MathWrapper {
+
+  // for x86 jdk8
+  public static double pow(double a, double b) {
+    return Math.pow(a, b);
+  }
+}
