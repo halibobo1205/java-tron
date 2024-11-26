90bc5c01c699911842ded801c64a92b652c20560
halibobo1205 <82020050+halibobo1205@users.noreply.github.com>
2024-11-26 16:54:38 +0800

    Create macOS.yml for test RocksDB

--- common/src/main/java/org/tron/core/config/args/Storage.java
+++ common/src/main/java/org/tron/core/config/args/Storage.java
@@ -175,12 +175,7 @@ public class Storage {
   private final Map<String, Sha256Hash> dbRoots = Maps.newConcurrentMap();
 
   public static String getDbEngineFromConfig(final Config config) {
-    if (Arch.isArm64() && !System.getenv().containsKey("CI")) {
-      logger.info("Arm64 architecture detected, using RocksDB as db engine, ignore config.");
-      return ROCKS_DB_ENGINE;
-    }
-    return config.hasPath(DB_ENGINE_CONFIG_KEY)
-        ? config.getString(DB_ENGINE_CONFIG_KEY) : DEFAULT_DB_ENGINE;
+    return ROCKS_DB_ENGINE;
   }
 
   public static Boolean getDbVersionSyncFromConfig(final Config config) {
