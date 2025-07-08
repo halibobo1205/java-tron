b1e1070bcc03eef2b7acc11ac265438f176b63e0
halibobo1205 <halibobo1205@gmail.com>
2025-07-08 16:01:29 +0800

    feat(toolkits): add query-properties

--- plugins/src/main/java/org/tron/plugins/Db.java
+++ plugins/src/main/java/org/tron/plugins/Db.java
@@ -14,6 +14,7 @@ import picocli.CommandLine;
         DbCopy.class,
         DbRoot.class,
         DbCheckSum.class,
+        DbQueryProperties.class,
     },
     commandListHeading = "%nCommands:%n%nThe most commonly used db commands are:%n"
 )
