00bb89314e56164d45fa0250aea39bb06c71f75b
halibobo1205 <halibobo1205@gmail.com>
2025-07-10 16:36:25 +0800

    feat(toolkits/correct ): correct BURN_TRX_AMOUNT again

--- plugins/src/main/java/org/tron/plugins/DbCorrect.java
+++ plugins/src/main/java/org/tron/plugins/DbCorrect.java
@@ -8,12 +8,11 @@ import org.rocksdb.RocksDBException;
 import org.tron.plugins.utils.ByteArray;
 import org.tron.plugins.utils.db.DBInterface;
 import org.tron.plugins.utils.db.DbTool;
-import org.tron.protos.Protocol;
 import picocli.CommandLine;
 
 @Slf4j(topic = "query")
 @CommandLine.Command(name = "correct",
-    description = "correct 4156a81439939809fdaaae7548a1de9c20783eb05b data.",
+    description = "correct BURN_TRX_AMOUNT data.",
     exitCodeListHeading = "Exit Codes:%n",
     exitCodeList = {
         "0:Successful",
@@ -29,12 +28,8 @@ public class DbCorrect implements Callable<Integer> {
   @CommandLine.Option(names = {"-h", "--help"}, help = true, description = "display a help message")
   private boolean help;
 
-  private static final byte[] address =
-      ByteArray.fromHexString("4156a81439939809fdaaae7548a1de9c20783eb05b");
-
   long diff  = 6798960;
 
-  String accountDB = "account";
   String peopertyDB = "properties";
 
   private static final byte[] BURN_TRX_AMOUNT = "BURN_TRX_AMOUNT".getBytes();
@@ -57,29 +52,12 @@ public class DbCorrect implements Callable<Integer> {
 
   private int correct() throws RocksDBException, IOException {
     try (
-        DBInterface acc = DbTool.getDB(this.path, accountDB);
         DBInterface prop = DbTool.getDB(this.path, peopertyDB)) {
-
-      Protocol.Account account =  Protocol.Account.parseFrom(acc.get(address));
-      if (account.getBalance() == 6830960) {
-        acc.put(address, account.toBuilder()
-            .setBalance(account.getBalance() - diff).build().toByteArray());
-        long burnAmount = ByteArray.toLong(prop.get(BURN_TRX_AMOUNT));
-        prop.put(BURN_TRX_AMOUNT, ByteArray.fromLong(burnAmount + diff));
-        long newBurnAmount = ByteArray.toLong(prop.get(BURN_TRX_AMOUNT));
-        long newBalance = Protocol.Account.parseFrom(acc.get(address)).getBalance();
-        spec.commandLine().getOut().format("Corrected account %s, new balance: %d, burn amount: %d%n",
-            ByteArray.toHexString(address), newBalance, newBurnAmount).println();
-      } else if (account.getBalance() == 6830960 - diff) {
-        spec.commandLine().getOut().format("Account %s already corrected, balance: %d%n",
-            ByteArray.toHexString(address), account.getBalance()).println();
-      } else {
-        spec.commandLine().getErr().println(spec.commandLine().getColorScheme()
-            .errorText(String.format("Account %s has unexpected balance: %d",
-                ByteArray.toHexString(address), account.getBalance())));
-        return 1;
-      }
-      return 0;
+      long burnAmount = ByteArray.toLong(prop.get(BURN_TRX_AMOUNT));
+      prop.put(BURN_TRX_AMOUNT, ByteArray.fromLong(burnAmount + diff));
+      long newBurnAmount = ByteArray.toLong(prop.get(BURN_TRX_AMOUNT));
+      spec.commandLine().getOut().format("Corrected burn amount: %d%n", newBurnAmount).println();
     }
+    return 0;
   }
 }
