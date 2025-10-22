0a26441be27636985492d258994feda87b772757
halibobo1205 <halibobo1205@gmail.com>
2025-10-22 15:31:14 +0800

    feat(db): force to rocksdb9

--- common/src/main/java/org/tron/core/config/args/Storage.java
+++ common/src/main/java/org/tron/core/config/args/Storage.java
@@ -174,8 +174,7 @@ public class Storage {
   private final Map<String, Sha256Hash> dbRoots = Maps.newConcurrentMap();
 
   public static String getDbEngineFromConfig(final Config config) {
-    return config.hasPath(DB_ENGINE_CONFIG_KEY)
-        ? config.getString(DB_ENGINE_CONFIG_KEY) : DEFAULT_DB_ENGINE;
+    return ROCKS_DB_ENGINE;
   }
 
   public static Boolean getDbVersionSyncFromConfig(final Config config) {
