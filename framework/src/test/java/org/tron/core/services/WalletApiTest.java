0dffbf98827fc507f06e6d98d3a2dbad491e86fe
halibobo1205 <halibobo1205@gmail.com>
2025-11-03 18:48:24 +0800

    debug(CI): add system.out for grpc-test

--- framework/src/test/java/org/tron/core/services/WalletApiTest.java
+++ framework/src/test/java/org/tron/core/services/WalletApiTest.java
@@ -52,11 +52,13 @@ public class WalletApiTest {
   @Before
   public void start() {
     logger.debug("========== Starting test: {} ==========", name.getMethodName());
+    System.out.println("========== Starting test: " + name.getMethodName() + " ==========");
   }
 
   @After
   public void end() throws InterruptedException {
     logger.debug("========== Ending test: {} ==========", name.getMethodName());
+    System.out.println("========== Ending test: " + name.getMethodName() + " ==========");
   }
 
   @Test
