5975fff73741a3ed8e74bcfc6d9e0aca6400a875
halibobo1205 <halibobo1205@gmail.com>
2026-05-12 15:39:40 +0800

    feat(build): support JDK 17 on x86

--- platform/src/main/java/common/org/tron/common/arch/Arch.java
+++ platform/src/main/java/common/org/tron/common/arch/Arch.java
@@ -62,27 +62,6 @@ public final class Arch {
     return osArch.contains("arm64") || osArch.contains("aarch64");
   }
 
-  public static boolean isX86() {
-    return !isArm64();
-  }
-
-  public static boolean isJava8() {
-    return javaSpecificationVersion().equals("1.8");
-  }
-
-  public static boolean isJava17() {
-    return javaSpecificationVersion().equals("17");
-  }
-
-  public static void throwIfUnsupportedJavaVersion() {
-    if ((isX86() && !isJava8()) || (isArm64() && !isJava17())) {
-      logger.info(withAll());
-      throw new UnsupportedOperationException(String.format(
-          "Java %s is required for %s architecture. Detected version %s", isX86() ? "1.8" : "17",
-          getOsArch(), javaSpecificationVersion()));
-    }
-  }
-
   public static void throwIfUnsupportedArm64Exception(String message) {
     if (isArm64()) {
       throw new UnsupportedOperationException(
